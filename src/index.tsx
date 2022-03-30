56b4fd8407d80e799dd0c4ed056d7742860e3e46
JJ <jj.vega86@gmail.com>
2022-03-29 21:28:47 -0400

    Installed react-query and added QueryClient to index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
 import { HabitProvider } from "./context/HabitContext";
 import { AuthProvider } from "./context/AuthContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <HabitProvider>
-          <App />
-        </HabitProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <HabitProvider>
+            <App />
+          </HabitProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
